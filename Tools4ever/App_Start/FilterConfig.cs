ea4a5176d8c4d8618a322a30148c33bf6e8dde6c
Marco W <37326977+marcido@users.noreply.github.com>
2018-12-14 09:52:38 +0100

    Add project files.

---
+++ Tools4ever/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Tools4ever
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
